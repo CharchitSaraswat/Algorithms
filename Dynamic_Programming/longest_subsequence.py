87cb39a0eccee3dc87daa20d2e31973cc73477b9
CharchitSaraswat <charchit91993@gmail.com>
2020-06-25 00:22:29 +0530

    len of lcs

--- Dynamic_Programming/longest_subsequence.py
+++ Dynamic_Programming/longest_subsequence.py
@@ -14,7 +14,10 @@ def fetch_lcs_length(x, y):
     for i in range(len(x)):
         for j in range(len(y)):
             if x[i] == y[j]:
-                c[i][j] = c[i-1][j-1] + 1
+                if i == 0 or j == 0:
+                    c[i][j] = 1
+                else:
+                    c[i][j] = c[i-1][j-1] + 1
                 b[i][j] = "m"
             elif c[i-1][j] > c[i][j-1]:
                 c[i][j] = c[i-1][j]
@@ -37,9 +40,10 @@ def generate_lcs(b,x,i,j):
         generate_lcs(b, x, i, j-1)
 
 if __name__ == "__main__":
+    # x = "daer"
+    # x = "bnck"
     x = "bread"
     y = "read"
-    lcs = ""
     c, b = fetch_lcs_length(x, y)
     generate_lcs(b, x, len(x)-1, len(y)-1)
     print(lcs[::-1])
